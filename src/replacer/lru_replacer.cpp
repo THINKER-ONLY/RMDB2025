844b3b1eaed6377894a8d11acdbe1c7c0c7c581c
THINKER-ONLY <3193304954@qq.com>
2025-06-28 12:02:42 +0800

    [feat]完成了replacer

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -27,7 +27,15 @@ bool LRUReplacer::victim(frame_id_t* frame_id) {
     // Todo:
     //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
     //  选择合适的frame指定为淘汰页面,赋值给*frame_id
-
+    if (LRUlist_.empty()
+    {
+        *frame_id = -1;
+        return false;
+    })
+    frame_id_t victim_frame = LRUlist_.back();
+    LRUlist_.pop_back();
+    LRUhash_.erase(victim_frame);
+    *frame_id = victim_frame;
     return true;
 }
 
@@ -40,6 +48,12 @@ void LRUReplacer::pin(frame_id_t frame_id) {
     // Todo:
     // 固定指定id的frame
     // 在数据结构中移除该frame
+    auto it = LRUhash_.find(frame_id);
+    if (it != LRUhash_.end())
+    {
+        LRUlist_.erase(it->second);
+        LRUhash_.erase(it);
+    }
 }
 
 /**
@@ -50,6 +64,13 @@ void LRUReplacer::unpin(frame_id_t frame_id) {
     // Todo:
     //  支持并发锁
     //  选择一个frame取消固定
+    std::scoped_lock lock{latch_};
+    if (LRUhash_.find(frame_id) != LRUhash_.end()) 
+    {
+        return;
+    }
+    LRUlist_.push_front(frame_id);
+    LRUhash_[frame_id] = LRUlist_.begin();
 }
 
 /**
